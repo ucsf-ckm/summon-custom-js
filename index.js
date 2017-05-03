431031abe94084bb6f1894770e94b88d7de7de53
tmcdev <tmcdev@users.noreply.github.com>
2017-05-03 14:35:44 -0700

    hiding redundant links in hamburger menu

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var node = document.createElement('style');
-node.innerHTML = '.siteLinks ul li:first-child{display:none;}';
+node.innerHTML = '.siteLinks ul li:first-child,.siteLinks .languageSwitcher{display:none;}'; //hide redundant links in hamburger menu
 
 // To add more CSS, use lines like this:
 // node.innerHTML += '.selector { font-size: large }';
