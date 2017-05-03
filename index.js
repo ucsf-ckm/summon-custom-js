cf2728b2960eca790c8696d90323517196560db6
tmcdev <tmcdev@users.noreply.github.com>
2017-05-03 16:16:14 -0700

    Hiding Wikipedia panel, showing the good stuff

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var node = document.createElement('style');
-node.innerHTML = '.siteLinks ul li:first-child,.siteLinks .languageSwitcher{display:none;}'; //hide redundant links in hamburger menu
+node.innerHTML = '.siteLinks ul li:first-child,.siteLinks .languageSwitcher{display:none;}'; // hide redundant links in hamburger menu
+node.innerHTML += '.content {display:none;}'; // hide wikipedia panel
+node.innerHTML += 'relatedLibrarian.content,.relatedTopics.content,.researchGuides.content {display:block;}' // show relevant panels
 
 // To add more CSS, use lines like this:
 // node.innerHTML += '.selector { font-size: large }';
