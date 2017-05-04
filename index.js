a71cd5a07d2c873c3ec3a291500c1ac554093812
tmcdev <tmcdev@users.noreply.github.com>
2017-05-04 10:23:06 -0700

    Disabling hamburger menu CSS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var node = document.createElement('style');
-node.innerHTML = '.siteLinks ul li:first-child,.siteLinks .languageSwitcher{display:none;}'; // hide redundant links in hamburger menu
+//node.innerHTML = '.siteLinks ul li:first-child,.siteLinks .languageSwitcher{display:none;}'; // hide redundant links in hamburger menu
 node.innerHTML += '.content {display:none;}'; // hide wikipedia panel
 node.innerHTML += '.relatedLibrarian.content,.relatedTopics.content,.researchGuides.content {display:block;}' // show relevant panels
 
