214c35f750be359db3aea720d46a93ecc551a721
Rich Trott <rtrott@gmail.com>
2017-06-08 22:56:27 -0700

    try more drastic/hackier measures to update banner link

--- index.js
+++ index.js
@@ -13,15 +13,17 @@ document.body.appendChild(node);
 // Fix off-campus login link to redirect to current search results after login.
 // We're going to depend on Summon's dependence on jQuery here.
 var fixBannerLink = function () {
-  // This is a terrible hack, but if FastClick exists, the Summon AngularJS app
-  // is loaded.
-  if (FastClick) {
-    $('.vpnBanner.customAuthBanner div a')
-      .attr('href', 'https://ucsf.idm.oclc.org/login?qurl=' + encodeURIComponent(location.href));
-  } else {
-    // Also a terrible hack: poll again in 100ms
+  links = $('.vpnBanner.customAuthBanner div a');
+  if (links.length === 0) {
     setTimeout(fixBannerLink, 100);
+    return;
   }
+  
+  links.each(function(idx) {
+    if (/^Off the UCSF network/.test(this.text)) {
+      this.href = 'https://ucsf.idm.oclc.org/login?qurl=' + encodeURIComponent(location.href);
+    }
+  });
 }
 
 $().ready(fixBannerLink);
