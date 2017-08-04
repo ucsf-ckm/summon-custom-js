caf8ce9fa5ec5d32db3e32664c5f39f1505947e7
tmcdev <tmcdev@users.noreply.github.com>
2017-08-04 11:24:42 -0700

    Trying again with Qualtrics slider

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ node.innerHTML += '.togglePreview:after {border-top-color#005380 !important;}'; 
 
 document.body.appendChild(node);
 
-// BEGIN QUALTRICS - even fresher from scratcher Intercept
+// BEGIN QUALTRICS - 8/4 slider
 var qualtricsNode = document.createElement('div');
-qualtricsNode.setAttribute('id', 'SI_5bUuj66mtC3j2eh');
+qualtricsNode.setAttribute('id', 'SI_1Fl2lq0f3xEqTkh');
 document.body.appendChild(qualtricsNode);
 (function(){var g=function(e,h,f,g){
 this.get=function(a){for(var a=a+"=",c=document.cookie.split(";"),b=0,e=c.length;b<e;b++){for(var d=c[b];" "==d.charAt(0);)d=d.substring(1,d.length);if(0==d.indexOf(a))return d.substring(a.length,d.length)}return null};
@@ -30,8 +30,7 @@ this.set=function(a,c){var b="",b=new Date;b.setTime(b.getTime()+6048E5);b="; ex
 this.check=function(){var a=this.get(f);if(a)a=a.split(":");else if(100!=e)"v"==h&&(e=Math.random()>=e/100?0:100),a=[h,e,0],this.set(f,a.join(":"));else return!0;var c=a[1];if(100==c)return!0;switch(a[0]){case "v":return!1;case "r":return c=a[2]%Math.floor(100/c),a[2]++,this.set(f,a.join(":")),!c}return!0};
 this.go=function(){if(this.check()){var a=document.createElement("script");a.type="text/javascript";a.src=g+ "&t=" + (new Date()).getTime();document.body&&document.body.appendChild(a)}};
 this.start=function(){var a=this;window.addEventListener?window.addEventListener("load",function(){a.go()},!1):window.attachEvent&&window.attachEvent("onload",function(){a.go()})}};
-try{(new g(100,"r","QSI_S_SI_5bUuj66mtC3j2eh","//zn0ombvsv6b6ofgif-ucsf.siteintercept.qualtrics.com/WRSiteInterceptEngine/?Q_SIID=SI_5bUuj66mtC3j2eh&Q_LOC="+encodeURIComponent(window.location.href))).start()}catch(i){}})();
-
+try{(new g(100,"r","QSI_S_SI_1Fl2lq0f3xEqTkh","//zn0ombvsv6b6ofgif-ucsf.siteintercept.qualtrics.com/WRSiteInterceptEngine/?Q_SIID=SI_1Fl2lq0f3xEqTkh&Q_LOC="+encodeURIComponent(window.location.href))).start()}catch(i){}})();
 // END QUALTRICS 
 
 // Fix off-campus login link to redirect to current search results after login.
